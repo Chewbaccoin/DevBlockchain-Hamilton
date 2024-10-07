3249f985c6f26e37f9c97b98d14c7fa8940d8aac
Chewbaccoin <nicolas@nbtc.fr>
2024-10-07 20:53:49 +0200

    Suite cours solidity
    Bank & Random contracts

---
+++ 2. Solidity/Exercices/bank.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+contract Random {  
+  uint private nonce; // = 0, affecté par défaut
+  
+   function random() public returns(uint) {
+       nonce++;
+       return  uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % 100;
+   }
+}
