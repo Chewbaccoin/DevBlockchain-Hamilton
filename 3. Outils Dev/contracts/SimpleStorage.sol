18992f2098330836d887d075544fc7726d15a858
Chewbaccoin <nicolas@nbtc.fr>
2024-10-10 18:47:32 +0200

    Cours sur HardHat

---
+++ 3. Outils Dev/contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+contract SimpleStorage {
+  uint256 private number;
+  function setNumber(uint256 _number) external {
+    number = _number;
+  }
+  function getNumber() external view returns(uint256) {
+    return number;
+  }
+}
